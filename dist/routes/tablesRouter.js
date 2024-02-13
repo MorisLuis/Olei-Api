6a8b665f5e5e88f26f3479cf0b16d141f0710976
Morado <moradoluisenrique@gmail.com>
2024-02-12 19:40:22 -0600

    setup

--- dist/routes/tablesRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const tables_1 = require("../controllers/tables");
-const router = (0, express_1.Router)();
-router.get("/", tables_1.getTables);
-exports.default = router;
-//# sourceMappingURL=tablesRouter.js.map
