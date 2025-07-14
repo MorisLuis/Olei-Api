be2499dec9231869f258cd59cf2dd5461e592505
“MorisLuis” <moradoluisenrique@gmail.com>
2025-07-14 17:45:16 -0600

    update( cobranza ) - update cobranza queries

--- src/database/querys/cobranza.ts
+++ src/database/querys/cobranza.ts
@@ -18,13 +18,14 @@ export const cobranzaQuery = {
             V.Id_Cliente,
             C.RazonSocial AS Nombre,
             V.Id_Almacen,
-            DATEDIFF(DAY, GETDATE(), V.FechaLiq) AS ExpiredDays,
+            DATEDIFF(DAY, GETDATE(), DATEADD(DAY, CD.Dias, V.Fecha)) AS ExpiredDays,
             V.Saldo,
             ISNULL(C.CorreoVtas, '') AS CorreoVtas
         FROM [dbo].[VENTAS] V
         JOIN [dbo].[CLIENTES] C ON C.Id_Cliente = V.Id_Cliente AND C.Id_Almacen = V.Id_Almacen
+        JOIN [dbo].[CONDVTAS] CD ON CD.Id_CondVta = V.Id_CondVta
         WHERE V.Saldo > 0
-        AND V.FechaLiq >= CAST(GETDATE() AS DATE)
+        AND V.Id_CondVta <> 1
         AND (@nombre = '' OR LOWER(C.RazonSocial) LIKE LOWER(@nombre) + '%');
         
         -- Totales por cliente y almacén
@@ -75,15 +76,16 @@ export const cobranzaQuery = {
             V.Id_Cliente,
             C.RazonSocial AS Nombre,
             V.Id_Almacen,
-            DATEDIFF(DAY, GETDATE(), V.FechaLiq) AS ExpiredDays,
+            DATEDIFF(DAY, GETDATE(), DATEADD(DAY, CD.Dias, V.Fecha)) AS ExpiredDays,
             V.Saldo,
             ISNULL(C.CorreoVtas, '') AS CorreoVtas
         FROM [dbo].[VENTAS] V
         JOIN [dbo].[CLIENTES] C ON C.Id_Cliente = V.Id_Cliente AND C.Id_Almacen = V.Id_Almacen
+        JOIN [dbo].[CONDVTAS] CD ON CD.Id_CondVta = V.Id_CondVta
         WHERE V.Saldo > 0
-        AND V.FechaLiq >= CAST(GETDATE() AS DATE)
+        AND V.Id_CondVta <> 1
         AND (@nombre = '' OR LOWER(C.RazonSocial) LIKE LOWER(@nombre) + '%');
-        
+            
         -- Totales por cliente y almacén
         WITH Totales AS (
             SELECT 
@@ -115,6 +117,7 @@ export const cobranzaQuery = {
             JOIN [dbo].[CLIENTES] C ON C.Id_Cliente = V.Id_Cliente AND C.Id_Almacen = V.Id_Almacen
             WHERE V.Saldo > 0
             AND V.FechaLiq >= CAST(GETDATE() AS DATE)
+            AND V.Id_CondVta <> 1
             AND LOWER(NOMBRE) LIKE '%' + LOWER(@nombre) + '%'
         )
         SELECT COUNT(*) AS TotalCount
@@ -130,94 +133,99 @@ export const cobranzaQuery = {
 
     getCobranzaByClient: `
         WITH
-            VENTAS_CTE
-            AS
-            (
-                SELECT
-                    CONCAT(Id_Almacen, '-', TipoDoc, '-', TRIM(Serie), '-', Folio) AS UniqueKey,
-                    Id_Cliente,
-                    Id_Almacen,
-                    TipoDoc,
-                    Folio,
-                    Serie,
-                    Fecha,
-                    FechaLiq,
-                    Saldo,
-                    Total,
-                    DATEDIFF(DAY, GETDATE(), FechaLiq) AS ExpiredDays
-                FROM [dbo].[VENTAS]
-                WHERE Id_Cliente = @Id_Cliente  AND Id_Almacen = @Id_Almacen
-                    AND Saldo > 0
-                    AND FechaLiq >= CAST(GETDATE() AS DATE) -- Condición para FechaLiq
-                    AND (@FilterTipoDoc = 0 OR (TipoDoc = @TipoDoc AND @FilterTipoDoc = 1))
-                    AND (@FilterExpired = 0 OR (DATEDIFF(DAY, GETDATE(), FechaLiq) < 0 AND @FilterExpired = 1))
-                    AND (@FilterNotExpired = 0 OR (DATEDIFF(DAY, GETDATE(), FechaLiq) > 0 AND @FilterNotExpired = 1))
-                    AND (@DateExactly IS NULL OR CAST(Fecha AS DATE) = @DateExactly)
-                    AND (@DateStart IS NULL OR CAST(Fecha AS DATE) >= @DateStart)
-                    AND (@DateEnd IS NULL OR CAST(Fecha AS DATE) <= @DateEnd)
-            )
-            SELECT *
-            FROM VENTAS_CTE
-                ORDER BY 
-                CASE 
-                    WHEN @OrderCondition = 'TipoDoc' THEN TipoDoc 
-                    WHEN @OrderCondition = 'Folio' THEN Folio 
-                    WHEN @OrderCondition = 'Fecha' THEN Fecha 
-                    WHEN @OrderCondition = 'ExpiredDays' THEN 
-                        CASE WHEN ExpiredDays IS NULL THEN 1 ELSE 0 END
-                END ASC,
-                CASE 
-                    WHEN @OrderCondition = 'TipoDoc' THEN Fecha 
-                    WHEN @OrderCondition = 'ExpiredDays' THEN 
-                        ExpiredDays -- Orden real de menor a mayor
-                    WHEN @OrderCondition = 'FechaLiq' THEN Fecha
-                END ASC,
+        VENTAS_CTE
+        AS
+        (
+            SELECT
+                CONCAT(Id_Almacen, '-', TipoDoc, '-', TRIM(Serie), '-', Folio) AS UniqueKey,
+                Id_Cliente,
+                Id_Almacen,
+                TipoDoc,
+                Folio,
+                Serie,
                 Fecha,
-                TipoDoc
-                OFFSET (@PageNumber - 1) * @PageSize ROWS
-                FETCH NEXT @PageSize ROWS ONLY
+                FechaLiq,
+                Saldo,
+                Total,
+                DATEDIFF(DAY, GETDATE(), DATEADD(DAY, CD.Dias, V.Fecha)) AS ExpiredDays
+        
+            FROM [dbo].[VENTAS] V
+            JOIN [dbo].[CONDVTAS] CD ON CD.Id_CondVta = V.Id_CondVta
+            WHERE Id_Cliente = @Id_Cliente  AND Id_Almacen = @Id_Almacen
+                AND Saldo > 0 AND V.Id_CondVta <> 1
+                AND FechaLiq >= CAST(GETDATE() AS DATE) -- Condición para FechaLiq
+                AND (@FilterTipoDoc = 0 OR (TipoDoc = @TipoDoc AND @FilterTipoDoc = 1))
+                AND (@FilterExpired = 0 OR (DATEDIFF(DAY, GETDATE(), FechaLiq) < 0 AND @FilterExpired = 1))
+                AND (@FilterNotExpired = 0 OR (DATEDIFF(DAY, GETDATE(), FechaLiq) > 0 AND @FilterNotExpired = 1))
+                AND (@DateExactly IS NULL OR CAST(Fecha AS DATE) = @DateExactly)
+                AND (@DateStart IS NULL OR CAST(Fecha AS DATE) >= @DateStart)
+                AND (@DateEnd IS NULL OR CAST(Fecha AS DATE) <= @DateEnd)
+        )
+        SELECT *
+        FROM VENTAS_CTE
+            ORDER BY 
+            CASE 
+                WHEN @OrderCondition = 'TipoDoc' THEN TipoDoc 
+                WHEN @OrderCondition = 'Folio' THEN Folio 
+                WHEN @OrderCondition = 'Fecha' THEN Fecha 
+                WHEN @OrderCondition = 'ExpiredDays' THEN 
+                    CASE WHEN ExpiredDays IS NULL THEN 1 ELSE 0 END
+            END ASC,
+            CASE 
+                WHEN @OrderCondition = 'TipoDoc' THEN Fecha 
+                WHEN @OrderCondition = 'ExpiredDays' THEN 
+                    ExpiredDays -- Orden real de menor a mayor
+                WHEN @OrderCondition = 'FechaLiq' THEN Fecha
+            END ASC,
+            Fecha,
+            TipoDoc
+            OFFSET (@PageNumber - 1) * @PageSize ROWS
+            FETCH NEXT @PageSize ROWS ONLY
+
     `,
 
     getCobranzaByClientTotal: `
-        WITH
-            VENTAS_CTE
-            AS
-            (
-                SELECT
-                    CONCAT(Id_Almacen, '-', TipoDoc, '-', TRIM(Serie), '-', Folio) AS UniqueKey,
-                    Id_Cliente,
-                    Id_Almacen,
-                    TipoDoc,
-                    Folio,
-                    Serie,
-                    Fecha,
-                    FechaLiq,
-                    Saldo,
-                    Total,
-                    DATEDIFF(DAY, GETDATE(), FechaLiq) AS ExpiredDays
-                FROM [dbo].[VENTAS]
-                WHERE Id_Cliente = @Id_Cliente  AND Id_Almacen = @Id_Almacen
-                    AND Saldo > 0
-                    AND FechaLiq >= CAST(GETDATE() AS DATE) -- Condición para FechaLiq
-                    AND (@FilterTipoDoc = 0 OR (TipoDoc = @TipoDoc AND @FilterTipoDoc = 1))
-                    AND (@FilterExpired = 0 OR (DATEDIFF(DAY, GETDATE(), FechaLiq) < 0 AND @FilterExpired = 1))
-                    AND (@FilterNotExpired = 0 OR (DATEDIFF(DAY, GETDATE(), FechaLiq) > 0 AND @FilterNotExpired = 1))
-                    AND (@DateExactly IS NULL OR CAST(Fecha AS DATE) = @DateExactly)
-                    AND (@DateStart IS NULL OR CAST(Fecha AS DATE) >= @DateStart)
-                    AND (@DateEnd IS NULL OR CAST(Fecha AS DATE) <= @DateEnd)
-            )
-            SELECT 
-                SUM(CASE WHEN ExpiredDays < 0 THEN Saldo ELSE 0 END) AS SumaSaldoVencido,
-                SUM(CASE WHEN ExpiredDays >= 0 OR ExpiredDays IS NULL THEN Saldo ELSE 0 END) AS SumaSaldoNoVencido,
-                SUM(Saldo) AS SumaTotalSaldo
-            FROM VENTAS_CTE;
+        WITH VENTAS_CTE
+        AS
+        (
+        SELECT
+            CONCAT(Id_Almacen, '-', TipoDoc, '-', TRIM(Serie), '-', Folio) AS UniqueKey,
+            Id_Cliente,
+            Id_Almacen,
+            TipoDoc,
+            Folio,
+            Serie,
+            Fecha,
+            FechaLiq,
+            Saldo,
+            Total,
+            DATEDIFF(DAY, GETDATE(), DATEADD(DAY, CD.Dias, V.Fecha)) AS ExpiredDays
+
+        FROM [dbo].[VENTAS] V
+        JOIN [dbo].[CONDVTAS] CD ON CD.Id_CondVta = V.Id_CondVta
+        WHERE Id_Cliente = @Id_Cliente  AND Id_Almacen = @Id_Almacen
+            AND Saldo > 0 AND V.Id_CondVta <> 1
+            AND FechaLiq >= CAST(GETDATE() AS DATE) -- Condición para FechaLiq
+            AND (@FilterTipoDoc = 0 OR (TipoDoc = @TipoDoc AND @FilterTipoDoc = 1))
+            AND (@FilterExpired = 0 OR (DATEDIFF(DAY, GETDATE(), FechaLiq) < 0 AND @FilterExpired = 1))
+            AND (@FilterNotExpired = 0 OR (DATEDIFF(DAY, GETDATE(), FechaLiq) > 0 AND @FilterNotExpired = 1))
+            AND (@DateExactly IS NULL OR CAST(Fecha AS DATE) = @DateExactly)
+            AND (@DateStart IS NULL OR CAST(Fecha AS DATE) >= @DateStart)
+            AND (@DateEnd IS NULL OR CAST(Fecha AS DATE) <= @DateEnd)
+        )
+        SELECT 
+            SUM(CASE WHEN ExpiredDays < 0 THEN Saldo ELSE 0 END) AS SumaSaldoVencido,
+            SUM(CASE WHEN ExpiredDays >= 0 OR ExpiredDays IS NULL THEN Saldo ELSE 0 END) AS SumaSaldoNoVencido,
+            SUM(Saldo) AS SumaTotalSaldo
+        FROM VENTAS_CTE;
     `,
 
     getCobranzaByClientCount: `
         SELECT COUNT(*) AS TotalCount
-        FROM [dbo].[VENTAS]
+        FROM [dbo].[VENTAS] V
+        JOIN [dbo].[CONDVTAS] CD ON CD.Id_CondVta = V.Id_CondVta
         WHERE Id_Cliente = @Id_Cliente  AND Id_Almacen = @Id_Almacen
-        AND Saldo > 0
+        AND Saldo > 0 AND V.Id_CondVta <> 1
         AND FechaLiq >= CAST(GETDATE() AS DATE)
         AND (@FilterTipoDoc = 0 OR (TipoDoc = @TipoDoc AND @FilterTipoDoc = 1))
         AND (@FilterExpired = 0 OR (DATEDIFF(DAY, GETDATE(), FechaLiq) < 0 AND @FilterExpired = 1))
