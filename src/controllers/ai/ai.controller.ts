33890143f203f2321eba54e569b18ab61d373c43
“MorisLuis” <moradoluisenrique@gmail.com>
2025-11-22 17:55:27 -0600

    Refactor askAI function to include headers in response and improve error handling

--- src/controllers/ai/ai.controller.ts
+++ src/controllers/ai/ai.controller.ts
@@ -4,26 +4,25 @@ import { generateSQLFromPrompt } from '../../services/ai/sqlPrompt.service';
 import { executeSQLQuery } from './utils/executeSQLQuery';
 import { isSafeSQL } from './utils/isSafeSQL';
 
-
 export const askAI = async (req: Request, res: Response) => {
     try {
         const { prompt } = req.body;
         if (!prompt) return res.status(400).json({ error: "Falta el prompt compa" });
 
         const sql = await generateSQLFromPrompt(prompt);
-        console.log({ sql})
 
         if (!isSafeSQL(sql)) {
             return res.status(400).json({ error: "Consulta SQL no segura" });
         }
         const userSession = req.sessionWeb;
         const data = await executeSQLQuery({ userSession, query: sql });
-
+        const headers = Object.keys(data[0])
 
         return res.json({
             ok: true,
-            sql,
-            data
+            total: data.length,
+            data,
+            headers
         });
     } catch (error) {
         console.error("❌ Error en askAI:", error);
