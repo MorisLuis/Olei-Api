946b93a66a83d5f2ac26ca0a32ca5dea793b7cb8
“MorisLuis” <moradoluisenrique@gmail.com>
2025-11-22 19:42:13 -0600

    Refactor askAI controller to use helper responses and improve error handling

--- src/controllers/ai/ai.controller.ts
+++ src/controllers/ai/ai.controller.ts
@@ -4,42 +4,31 @@ import { generateSQLFromPrompt } from '../../services/ai/sqlPrompt.service';
 import { executeSQLQuery } from './utils/executeSQLQuery';
 import { isSafeSQL } from './utils/isSafeSQL';
 import { classifyAIResponse } from './utils/classifier';
+import { errorResponse, successResponse } from '../../helpers/response';
 
 export const askAI = async (req: Request, res: Response) => {
     try {
         const { prompt } = req.body;
-        if (!prompt) return res.status(400).json({ error: "Falta el prompt compa" });
+        if (!prompt) return errorResponse(res, 'Falta el promtp', 400);
 
         const aiText = await generateSQLFromPrompt(prompt);
         const type = classifyAIResponse(aiText);
 
-        console.log("🤖 Respuesta de la IA:", { type, aiText });
-
         if (type !== "SQL") {
-            return res.json({
-                ok: false,
-                type,
-                message: aiText
-            });
+            return errorResponse(res, aiText, 400);
         }
 
         if (!isSafeSQL(aiText)) {
-            return res.status(400).json({ error: "Consulta SQL no segura" });
+            return errorResponse(res, 'Consulta SQL no segura', 400);
         }
+
         const userSession = req.sessionWeb;
         const data = await executeSQLQuery({ userSession, query: aiText });
         const headers = Object.keys(data[0] ? data[0] : {});
 
-        return res.json({
-            ok: true,
-            total: data.length,
-            data,
-            headers,
-            query: aiText
-        });
+        return successResponse(req, res, { data, type, query: aiText, headers }, "Consulta AI exitosa", 200, { totals: { show: data.length, total: data.length }, pages: { current: 1, totalPages: 1 } });
 
     } catch (error) {
-        console.error("❌ Error en askAI:", error);
         return res.status(500).json({ error: "Error del servidor compa" });
     }
 };
