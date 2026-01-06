74e9a3e2d85832db4d7e6c4829cbe6bf35b8a198
“MorisLuis” <moradoluisenrique@gmail.com>
2026-01-05 20:54:08 -0600

    Refactor postInformesiaValidations to remove unused fields and improve validation structure

--- src/validations/informesiaValidations.ts
+++ src/validations/informesiaValidations.ts
@@ -5,7 +5,10 @@ import z from "zod";
 export const postInformesiaValidations = z.object({
     Titulo: z.string().min(1, { message: "El título es obligatorio" }),
     Categoria: z.number().int().positive({ message: "La categoría debe ser un número positivo" }),
-    Descripcion: z.string().optional(),
-    PeticionUsuario: z.string().optional(),
-    SQL: z.string().min(1, { message: "El SQL es obligatorio" }),
+    Descripcion: z.string().optional()
+})
+
+
+export const postInformesiaParamsValidations = z.object({
+    queryId: z.string().uuid({ message: "El queryId debe ser un UUID válido" })
 })
