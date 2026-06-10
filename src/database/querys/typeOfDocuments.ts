1bbedcd57ba9d13a723cdbbefbb0a8557eb786d0
“MorisLuis” <moradoluisenrique@gmail.com>
2026-06-10 14:14:18 -0600

    fix(typeOfDocuments) - Update getTypeOfDocuments query to filter TipoDoc values to 2, 3, and 4

--- src/database/querys/typeOfDocuments.ts
+++ src/database/querys/typeOfDocuments.ts
@@ -5,7 +5,7 @@ export const typeOfDocumentsQuery = {
     getTypeOfDocuments: `
         SELECT [TipoDoc] ,[Nombre]
         FROM [dbo].[TIPODOCS]
-        WHERE TipoDoc IN (1,2,3)
+        WHERE TipoDoc IN (2,3,4)
     `
 
 }
