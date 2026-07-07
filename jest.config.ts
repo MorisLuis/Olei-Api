cc91b3f991d957f83eaefd37ea5db3d9562f6d1a
“MorisLuis” <moradoluisenrique@gmail.com>
2026-07-07 14:18:43 -0600

    Add health check test and update Jest configuration formatting

--- jest.config.ts
+++ jest.config.ts
@@ -1,27 +1,30 @@
-// jest.config.ts
-import type { Config } from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    rootDir: './',
+    preset: "ts-jest",
+    testEnvironment: "node",
+    rootDir: "./",
     testMatch: [
-        '**/__tests__/**/*.test.ts',
-        '**/__tests__/**/*.int.test.ts'
+        "**/__tests__/**/*.test.ts",
+        "**/__tests__/**/*.int.test.ts"
     ],
-    moduleDirectories: ['node_modules', 'src'],
-    moduleFileExtensions: ['ts', 'js', 'json'],
-    setupFilesAfterEnv: ['<rootDir>/__tests__/setup/jest.setup.ts'],
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.json'
-        }
+    moduleDirectories: ["node_modules", "src"],
+    moduleFileExtensions: ["ts", "js", "json"],
+    setupFilesAfterEnv: ["<rootDir>/__tests__/setup/jest.setup.ts"],
+    transform: {
+        "^.+\\.ts$": ["ts-jest", {
+            tsconfig: "tsconfig.json"
+        }]
     },
     verbose: true,
     clearMocks: true,
     resetMocks: true,
     automock: false,
-
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!src/**/*.d.ts",
+        "!src/index.ts"
+    ]
 };
 
 export default config;
