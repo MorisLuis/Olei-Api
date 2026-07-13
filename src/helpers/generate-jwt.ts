fc5094e37945b663cced1f6fa8b07ba351aeae69
“MorisLuis” <moradoluisenrique@gmail.com>
2026-07-12 23:52:25 -0600

    Refactor JWT token generation functions to improve secret handling and error checking

--- src/helpers/generate-jwt.ts
+++ src/helpers/generate-jwt.ts
@@ -1,29 +1,22 @@
 import jwt from 'jsonwebtoken';
 
-const ACCESS_TOKEN_SEVER_SECRET = process.env.ACCESS_TOKEN_SEVER_SECRET || 'access_server_secret';
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'access_secret';
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || 'refresh_secret';
-
-/* MOBILE */
-export const generateAccessTokenServer = (sessionId: string): string => {
-    return jwt.sign({ sessionId }, ACCESS_TOKEN_SEVER_SECRET, { expiresIn: '1y' });
-};
-
-export const generateAccessToken = (sessionId: string): string => {
-    return jwt.sign({ sessionId }, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
-};
-
-export const generateRefreshToken = (sessionId: string): string => {
-    return jwt.sign({ sessionId }, REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
-};
-
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
 
 /* WEB */
 export const generateAccessTokenWeb = (sessionId: string): string => {
-    return jwt.sign({ sessionId }, 'access_secret', { expiresIn: '1d' });
+    if (!ACCESS_TOKEN_SECRET) {
+        throw new Error('Missing ACCESS_TOKEN_SECRET');
+    }
+
+    return jwt.sign({ sessionId }, ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
 };
 
 export const generateRefreshTokenWeb = (sessionId: string): string => {
+    if (!REFRESH_TOKEN_SECRET) {
+        throw new Error('Missing REFRESH_TOKEN_SECRET');
+    }
+
     return jwt.sign({ sessionId }, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
 };
