9c523a8bd33acd7e0f8e77a845c3880b9d71cb10
“MorisLuis” <moradoluisenrique@gmail.com>
2026-07-20 16:54:42 -0600

    Refactor product controller imports and enhance documentation for clarity

--- src/controllers/products/products.ts
+++ src/controllers/products/products.ts
@@ -2,7 +2,7 @@ import type { NextFunction, Request, Response } from 'express'
 import { dbConnection } from '../../database';
 import { productsWebQuerys } from '../../database/querys/productsWeb';
 import { getProductByStockAndCodeBarSchema, getProductsByStockQuerySchema } from '../../validations/productsValidations';
-import { getProductByStockAndCodeBarService, getProductsByStockService, searchProductByStockService } from '../../services/productsServices';
+import { getProductByStockAndCodeBarService, getProductsByStockService, searchProductByStockService } from '../../services/products/index';
 import { UnauthorizedError, ValidationError } from '../../errors/CustomError';
 import { searchProductInventoryQuerySchema } from '../../validations/inventoryValidations';
 
@@ -48,6 +48,13 @@ const getProducById = async (req: Request, res: Response, next: NextFunction): P
     }
 }
 
+
+/**
+ * @description Controller to get products by stock with pagination and optional total count.
+ * @route GET /products/byStock
+ * @client Mobile App
+ */
+
 const getProductsByStock = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
 
     try {
@@ -72,6 +79,12 @@ const getProductsByStock = async (req: Request, res: Response, next: NextFunctio
     }
 };
 
+/**
+ * @description Controller to get the total count of products by stock.
+ * @route GET /products/byStockCount
+ * @client Mobile App
+ */
+
 const getTotalOfProductsByStock = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
     try {
         const userSession = req.session;
@@ -87,6 +100,12 @@ const getTotalOfProductsByStock = async (req: Request, res: Response, next: Next
         next(error);
     }
 };
+
+/**
+ * @description Controller to get a product by stock and code bar.
+ * @route GET /products/byStockAndCodeBar
+ * @client Mobile App
+ */
 
 const getProductByStockAndCodeBar = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
 
@@ -108,6 +127,12 @@ const getProductByStockAndCodeBar = async (req: Request, res: Response, next: Ne
     }
 };
 
+/**
+ * @description Controller to search products in inventory ( products ) by search term.
+ * @route GET /inventory/search/product
+ * @client Mobile App
+ */
+
 const searchProductInventory = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
 
     try {
@@ -126,6 +151,13 @@ const searchProductInventory = async (req: Request, res: Response, next: NextFun
         return next(error);
     }
 };
+
+
+/**
+ * @description Controller to search products in inventory ( products ) without code bar.
+ * @route GET /inventory/search/product/withoutcodebar
+ * @client Mobile App
+ */
 
 const searchProductInventoryWithoutCodebar = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
 
