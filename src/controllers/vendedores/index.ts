c66e412eef0991a87f4b25599cad7146d5bd09b8
“MorisLuis” <moradoluisenrique@gmail.com>
2026-08-04 00:17:53 -0600

    Add controllers for retrieving vendors and vendor by ID

---
+++ src/controllers/vendedores/index.ts
@@ -0,0 +1,50 @@
+import type { NextFunction, Request, Response } from "express";
+import { successResponse } from "../../helpers/response";
+import { getVendedorByIdService, getVendedoresService } from "../../services/vendedores";
+import { getVendedorByIdParamsSchema, getVendedoresQuerySchema } from "./vendedores.schema";
+
+export const getVendedores = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<Response | void> => {
+    try {
+        const { PageNumber, PageSize } = getVendedoresQuerySchema.parse(req.query);
+        const { vendedores, total } = await getVendedoresService({
+            userSession: req.session,
+            PageNumber,
+            PageSize,
+        });
+
+        return successResponse(req, res, vendedores, "Operation successful", 200, {
+            totals: {
+                show: vendedores.length,
+                total,
+            },
+            pages: {
+                current: PageNumber,
+                totalPages: Math.ceil(total / PageSize),
+            },
+        });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+export const getVendedorById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<Response | void> => {
+    try {
+        const { id: Id_Vendedor } = getVendedorByIdParamsSchema.parse(req.params);
+        const { vendedor } = await getVendedorByIdService({
+            userSession: req.session,
+            Id_Vendedor,
+        });
+
+        return successResponse(req, res, vendedor);
+    } catch (error) {
+        return next(error);
+    }
+};
