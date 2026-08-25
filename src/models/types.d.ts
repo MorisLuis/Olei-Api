d04a78d5a470404117c4e0f3cbb55e09ce71f806
“MorisLuis” <moradoluisenrique@gmail.com>
2026-08-25 12:49:48 -0600

    feat: update ServerDependencies interface to include closeRedis, stopCleanupTimer, and closeHttpServer

--- src/models/types.d.ts
+++ src/models/types.d.ts
@@ -1,9 +1,12 @@
-import type { listen } from "./server";
+import type { closeHttpServer, listen } from "./server";
 
 export interface ServerDependencies {
     connectDatabase: () => Promise<unknown>;
     connectRedis: () => Promise<void>;
     closeDatabase: () => Promise<void>;
+    closeRedis: () => Promise<void>;
     abortRedis: () => void;
+    stopCleanupTimer: () => void;
+    closeHttpServer: typeof closeHttpServer;
     listen: typeof listen;
 }
