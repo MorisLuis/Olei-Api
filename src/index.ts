4ea0ce9068297e0d89341a613e8fdf90c03406ce
“MorisLuis” <moradoluisenrique@gmail.com>
2026-08-24 21:12:18 -0600

    feat: refactor application startup to use bootstrap function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 /// <reference path="./types/express-session.d.ts" />
 
-import Server from "./models/server";
-import dotenv from "dotenv";
+import { bootstrap } from './bootstrap';
 
-dotenv.config()
-
-const server = new Server();
-server.listen();
-
-export default server;
+void bootstrap().catch(() => {
+    console.error('Application startup failed');
+    process.exitCode = 1;
+});
